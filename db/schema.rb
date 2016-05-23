af06e47055046d05e21c9e281383eaaebeea1acf
rubyJ23 <til.sch13@gmail.com>
2016-05-23 08:04:16 +0000

    Aufgaben als erlidigt markiren hinzugefügt

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511073055) do
+ActiveRecord::Schema.define(version: 20160511124214) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.datetime "completed_at"
   end
 
 end
